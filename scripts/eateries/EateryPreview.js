41e9ec67d2700f32da9e3199ac5e3c6b1f0a3ac9
estherviolin <esthernaomi.violin@gmail.com>
2020-08-05 11:34:45 -0500

    mid preview

--- scripts/eateries/EateryPreview.js
+++ scripts/eateries/EateryPreview.js
@@ -1,3 +1,5 @@
+import {useEateries, getEateries} from "./EateryProvider.js"
+
 const contentTarget = document.querySelector(".attractionPreviewContainer")
 const eventHub = document.querySelector(".container")
 
@@ -5,7 +7,7 @@ const eventHub = document.querySelector(".container")
 
 export const EateryPreview = () => {
     //listen for custom event from eatery selects
-    eventHub.addEventListener("")
+    eventHub.addEventListener("eaterySelected")
 
 
 
