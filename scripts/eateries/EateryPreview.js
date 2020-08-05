1a43c5714869137737ccc5612ebfcba18d28d5d2
estherviolin <esthernaomi.violin@gmail.com>
2020-08-05 10:36:59 -0500

    started eaterypreview

---
+++ scripts/eateries/EateryPreview.js
@@ -0,0 +1,12 @@
+const contentTarget = document.querySelector(".attractionPreviewContainer")
+const eventHub = document.querySelector(".container")
+
+
+
+export const EateryPreview = () => {
+    //listen for custom event from eatery selects
+    eventHub.addEventListener("")
+
+
+
+}
