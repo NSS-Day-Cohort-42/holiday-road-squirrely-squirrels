a0822e70fe102ff2dbabbc68c421a60828ba7bb6
Raparker88 <raparker88@gmail.com>
2020-08-05 14:01:24 -0500

    added event listener for dialog modal and fixed index.html

--- scripts/attractions/AttractionPreview.js
+++ scripts/attractions/AttractionPreview.js
@@ -1,6 +1,7 @@
 import { useOddities } from "./AttractionProvider.js"
 
 const eventHub = document.querySelector(".container")
+const contentTarget = document.querySelector(".preview--oddity")
 
 eventHub.addEventListener("odditySelected", (event) => {
     oddityArray = useOddities()
@@ -9,23 +10,31 @@ eventHub.addEventListener("odditySelected", (event) => {
     })
     render(oddityChosen)
 })
+
 eventHub.addEventListener("click", (event) => {
-    
+    if (event.target.id.startsWith("detail--")){
+        const [ prompt, oddityId ] = event.target.id.split("--")
+        const contentTarget = document.querySelector(`.dialog--${oddityId}`)
+        contentTarget.showModal()
+    }
+    else if (event.target.id === `close--oddityId`){
+        const theDialog = clickEvent.target.parentNode
+        theDialog.close()
+    }
 })
 
 
 const render = (oddityObj) => {
     contentTarget.innerHTML += `
-        <section class="preview preview--oddity">
             <h3>${oddityObj.name}</h3>
             <p>${oddityObj.city}, ${oddityObj.state}</p> 
-            <button id="detail--${oddityObj.id}">Description</button>
-            <dialog class="dialog--${oddityObj.id}>
+            <button class="detailButton" id="detail--${oddityObj.id}">Description</button>
+            <dialog class="dialog dialog--${oddityObj.id}>
+            <h4>${oddityObj.name}</h4>
             <p>${oddityObj.description}</p>
             <p>souvenirs: ${oddityObj.amenities.souvenirs}</p>
             <p>restrooms: ${oddityObj.amenities.restrooms}</p>
+            <button class="close" id="close--${oddityobj.id}">Close</button>
             </dialog>
-        </section>
             `
-            
 }
