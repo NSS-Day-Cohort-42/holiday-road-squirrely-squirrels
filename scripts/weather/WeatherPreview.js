08b0d07d5ae97668e02cf2ac06a389635e1e6620
mgjeffries <gib.jeffries@gmail.com>
2020-08-05 13:53:20 -0500

    remove hard coded location

--- scripts/weather/WeatherPreview.js
+++ scripts/weather/WeatherPreview.js
@@ -4,9 +4,6 @@ import { getParkCoordinates } from "../parks/ParkProvider.js"
 const contentTarget = document.querySelector(".weatherContainer")
 const eventHub = document.querySelector(".container")
 
-let latitudeNashville = 36.16589
-let longitudeNashville = -86.78444
-
 eventHub.addEventListener("parkSelected", customEvent => {
   // get park ID
   const parkId = customEvent.detail.parkId
@@ -20,18 +17,6 @@ eventHub.addEventListener("parkSelected", customEvent => {
       render(weather)
     })
 })
-
-
-
-export const NashvileWeatherPreview = () => {
-  getWeather( latitudeNashville, longitudeNashville )
-    .then( () => {
-      const weather = useWeather()
-      render(weather)
-      }
-    )
-}
-
 
 const kelvinToFarenheit = ( tempKelvin ) => {
   return Math.round((tempKelvin-273.15)*(9/5)+32)
