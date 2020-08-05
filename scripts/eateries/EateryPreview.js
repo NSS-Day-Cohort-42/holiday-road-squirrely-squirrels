c0944b03e9335e9b11597605741d9288aa3d6006
estherviolin <esthernaomi.violin@gmail.com>
2020-08-05 15:50:59 -0500

    added eatery preview

---
+++ scripts/eateries/EateryPreview.js
@@ -0,0 +1,28 @@
+const contentTarget = document.querySelector(".preview--eatery")
+const eventHub = document.querySelector(".container")
+
+eventHub.addEventListener("eaterySelected", (customEvent) => {
+    const eaterySelected = customEvent.detail.eateryId
+
+    render(eaterySelected)
+})
+
+
+const render = (selectedEateryObj) => {
+    contentTarget.innerHTML = `
+    <h3>${selectedEateryObj.businessName}</h3>
+    <div>${selectedEateryObj.city}, ${selectedEateryObj.state}</div>
+    <button class="detailButton" id="detail--${selectedEateryObj.id}">Details</button>
+    <dialog class="dialog dialog--${selectedEateryObj.id}">
+        <h4>${selectedEateryObj.businessName}</h4>
+        <p>${selectedEateryObj.description}</p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+
+        <button class="closeButton" id="close--${selectedEateryObj.id}"</button>
+    </dialog>
+     `
+}
