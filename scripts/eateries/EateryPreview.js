463ec7ef560244d4d12e082f6419badbab02701f
estherviolin <esthernaomi.violin@gmail.com>
2020-08-05 11:56:22 -0500

    adding park name and location

--- scripts/eateries/EateryPreview.js
+++ scripts/eateries/EateryPreview.js
@@ -7,8 +7,34 @@ const eventHub = document.querySelector(".container")
 
 export const EateryPreview = () => {
     //listen for custom event from eatery selects
-    eventHub.addEventListener("eaterySelected")
+    eventHub.addEventListener("eaterySelected", (customEvent) => {
 
+        const eaterySelected = customEvent.detail.eateryId
 
+        const allEateries = useEateries()
 
+        const selectedEatery = allEateries.find(
+            (eateryObj) => {
+                return eaterySelected === eateryObj.id
+            }
+        ) //returns one object with matching id
+
+        //add to DOM
+        render 
+    })
+
+}
+
+//render function
+
+const render = (selectedEateryObj) = {
+    return `
+    <section class="preview preview--eatery">
+        <h3>${selectedEateryObj.businessName}</h3>
+        <div>${selectedEateryObj.city} , ${selectedEateryObj.state}</div>
+        <button id="${selectedEateryObj.id}">Details
+
+    </section>
+    `
+   
 }
