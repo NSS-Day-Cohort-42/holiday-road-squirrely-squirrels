aa95de8fd58961412f65de003ed8fd916b040bfb
mgjeffries <gib.jeffries@gmail.com>
2020-08-05 15:02:25 -0500

    add score rank and filter to eateries

--- scripts/eateries/EateryProvider.js
+++ scripts/eateries/EateryProvider.js
@@ -11,7 +11,45 @@ export const getEateries = () => {
         .then(response => response.json())
         .then(
             parsedEateries => {
+
                 eateries = parsedEateries
+                scoreEateries()
+                rankEateries()
+                filterEateries()
             }
         )
+}
+
+export const scoreEateries = () => {
+    for (const eatery of eateries) {
+        let score = 0
+        if (eatery.ameneties.petFriendly) {
+            score++
+        }
+        if (eatery.ameneties.wifi) {
+            score++
+        }
+        if (eatery.ameneties.diaperFacility) {
+            score++
+        }
+        if (eatery.ameneties.playground) {
+            score++
+        }
+        if (eatery.ameneties.restrooms) {
+            score++
+        }
+        eatery.profileScore = score
+    }
+}
+
+const rankEateries = () => {
+    eateries.sort( (currentElement, nextElement) => {
+        return nextElement.profileScore - currentElement.profileScore
+    })
+}
+
+const filterEateries = () => {
+    eateries = eateries.filter( eatery => {
+        return eatery.profileScore >= 3
+    })
 }
