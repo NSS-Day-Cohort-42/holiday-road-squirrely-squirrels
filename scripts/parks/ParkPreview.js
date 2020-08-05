4031cb1dddb409967f8ba4181a00fb5463264121
Jacob Eckert <jweckert17@gmail.com>
2020-08-05 16:07:12 -0500

    got dialog modal working

--- scripts/parks/ParkPreview.js
+++ scripts/parks/ParkPreview.js
@@ -3,12 +3,25 @@ import { useParks } from "./ParkProvider.js"
 const eventHub = document.querySelector(".container")
 const contentTarget = document.querySelector(".preview--park")
 
+contentTarget.addEventListener("click", event => {
+  if(event.target.id.startsWith("parkDetail--")) {
+    const parkId = event.target.id.split("--")[1]
+
+    const dialogNode = document.querySelector(`#parkDialog--${parkId}`)
+    dialogNode.showModal()
+  }
+  else if(event.target.classList.contains("closeButton")) {
+    const dialogNode = event.target.parentNode
+    dialogNode.close()
+  }
+})
+
 const render = park => {
   contentTarget.innerHTML = `
     <h3 class="preview__header">${park.name}</h3>
     <div class="preview__location">${park.addresses[0].city}, ${park.addresses[0].stateCode}</div>
-    <button class="detailButton" id="detail--${park.id}">Details</button>
-    <dialog class="dialog dialog--park">
+    <button class="detailButton" id="parkDetail--${park.id}">Details</button>
+    <dialog class="dialog dialog--park" id="parkDialog--${park.id}">
       <h4 class="preview-dialog__header">${park.name}</h4>
       <p class="preview-dialog__description">${park.description}</p>
       <button class="closeButton" id="close--${park.id}">Close</button>
