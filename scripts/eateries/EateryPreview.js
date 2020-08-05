56741e2d324e4ff5ae6ba5046390b10e6469e5e4
estherviolin <esthernaomi.violin@gmail.com>
2020-08-05 13:59:41 -0500

    added everything to dialog box

--- scripts/eateries/EateryPreview.js
+++ scripts/eateries/EateryPreview.js
@@ -1,41 +1,50 @@
-import {useEateries, getEateries} from "./EateryProvider.js"
+import { useEateries, getEateries } from "./EateryProvider.js"
 
-const contentTarget = document.querySelector(".attractionPreviewContainer")
+const contentTarget = document.querySelector(".preview--eatery")
 const eventHub = document.querySelector(".container")
 
 
 
-export const EateryPreview = () => {
-    //listen for custom event from eatery selects
-    eventHub.addEventListener("eaterySelected", (customEvent) => {
 
-        const eaterySelected = customEvent.detail.eateryId
+//listen for custom event from eatery selects
+eventHub.addEventListener("eaterySelected", (customEvent) => {
 
-        const allEateries = useEateries()
+    const eaterySelected = customEvent.detail.eateryId
 
-        const selectedEatery = allEateries.find(
-            (eateryObj) => {
-                return eaterySelected === eateryObj.id
-            }
-        ) //returns one object with matching id
+    const allEateries = useEateries()
 
-        //add to DOM
-        render 
-    })
+    const selectedEatery = allEateries.find(
+        (eateryObj) => {
+            return eaterySelected === eateryObj.id
+        }
+    ) //returns one object with matching id
 
-}
+    //add to DOM
+    render
+})
+
+
 
 //render function
 
-const render = (selectedEateryObj) = {
-    return `
-    <section class="preview preview--eatery">
+const render = (selectedEateryObj) => {
+    contentTarget.innerHTML = `
         <h3>${selectedEateryObj.businessName}</h3>
-        <div>${selectedEateryObj.city} , ${selectedEateryObj.state}</div>
-        <button id="detail--${selectedEateryObj.id}">Details</button>
-        <dialog class=
+        <div>${selectedEateryObj.city}, ${selectedEateryObj.state}</div>
+        <button class="detailButton" id="detail--${selectedEateryObj.id}">Details</button>
+        <dialog class="dialog dialog--${selectedEateryObj.id}">
+            <h4>${selectedEateryObj.businessName}</h4>
+            <p>${selectedEateryObj.description}</p>
+            <p>${selectedEateryObj.ameneties.petFriendly}</p>
+            <p>${selectedEateryObj.ameneties.wifi}</p>
+            <p>${selectedEateryObj.ameneties.diaperFacility}</p>
+            <p>${selectedEateryObj.ameneties.playground}</p>
+            <p>${selectedEateryObj.ameneties.restrooms}</p>
 
-    </section>
+            <button class="closeButton" id="close--${selectedEateryObj.id}">Close</button
+
+        </dialog>
+           
     `
-   
+
 }
