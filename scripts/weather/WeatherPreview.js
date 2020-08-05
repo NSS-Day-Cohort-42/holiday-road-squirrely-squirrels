0db0cbdec9d03f91e535285e1d72127018d57a11
mgjeffries <gib.jeffries@gmail.com>
2020-08-05 11:40:25 -0500

    add event listener to weather preview

--- scripts/weather/WeatherPreview.js
+++ scripts/weather/WeatherPreview.js
@@ -1,14 +1,25 @@
 import { getWeather, useWeather } from "./WeatherProvider.js"
 
 const contentTarget = document.querySelector(".weatherContainer")
+const eventHub = document.querySelector(".container")
+
+eventHub.addEventListener("parkSelected", customEvent => {
+  // get park ID
+
+  // get latitude and longitude of park
+
+  // get weather for the park
+
+  // display weather for the park
+})
 
 getWeather()
   .then( () => {
     const weather = useWeather()
-    contentTarget.innerHTML = `the current temperature in Nashville is ${kelvinToFarenheit(weather.current.temp)} degrees F`
+    contentTarget.innerHTML = `The current temperature in Nashville is ${kelvinToFarenheit(weather.current.temp)} degrees F`
   }
 )
 
-const kelvinToFarenheit = ( tempKelvin) => {
+const kelvinToFarenheit = ( tempKelvin ) => {
   return Math.round((tempKelvin-273.15)*(9/5)+32)
 }
