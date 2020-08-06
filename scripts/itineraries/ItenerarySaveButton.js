8ef41810e3b56bd7283845750dcae3086b191446
mgjeffries <gib.jeffries@gmail.com>
2020-08-06 14:14:26 -0500

    add event listener for save clicked

--- scripts/itineraries/ItenerarySaveButton.js
+++ scripts/itineraries/ItenerarySaveButton.js
@@ -13,23 +13,43 @@ eventHub.addEventListener("parkSelected", customEvent => {
   }
 
 })
-// eventHub.addEventListener("odditySelected", customEvent => {
+eventHub.addEventListener("odditySelected", customEvent => {
   
-// })
-// eventHub.addEventListener("eaterySelected", customEvent => {
+  if ( customEvent.detail.oddityId !== "" ) {
+    isOdditySelected = true
+    render()
+  }
+
+})
+eventHub.addEventListener("eaterySelected", customEvent => {
   
-// })
+  if ( customEvent.detail.eateryId !== "" ) {
+    isEaterySelected = true
+    render()
+  }
+
+})
+
+eventHub.addEventListener("click", clickEvent => {
+  if (clickEvent.target.id === "save-itenerary-button") {
+    console.log('save Itenerary clicked')
+    if (readyToSave()) {
+      // save itenerary to provider
+      console.log("saved")
+    }
+  }
+})
 
 export const ItenerarySaveButton = () => {
   render()
 }
 
 const readyToSave = () => {
-  return isParkSelected
+  return isParkSelected && isEaterySelected && isOdditySelected
 }
 
 const render = () => {
   contentTarget.innerHTML = `
-  <button>Save Itenerary : ready to Save: ${readyToSave()}</button>
+  <button id="save-itenerary-button">Save Itenerary : ready to Save: ${readyToSave()}</button>
   `
 }
