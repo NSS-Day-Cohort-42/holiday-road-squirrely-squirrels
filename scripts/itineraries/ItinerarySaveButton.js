07aa3a2eec1923da2b8029bb2e7486406292ed6e
mgjeffries <gib.jeffries@gmail.com>
2020-08-06 15:34:10 -0500

    fix conditions in event listeners

--- scripts/itineraries/ItinerarySaveButton.js
+++ scripts/itineraries/ItinerarySaveButton.js
@@ -17,31 +17,40 @@ let itineraryData = {
 
 eventHub.addEventListener("parkSelected", customEvent => {
   
-  if ( customEvent.detail.parkId !== "" ) {
+  if ( customEvent.detail.parkId !== "0" ) {
     isParkSelected = true
     itineraryData.parkId = customEvent.detail.parkId
     itineraryData.parkName = customEvent.detail.parkName
     render()
   }
+  else {
+    isParkSelected = false
+  }
 
 })
 eventHub.addEventListener("odditySelected", customEvent => {
   
-  if ( customEvent.detail.oddityId !== "" ) {
+  if ( customEvent.detail.oddityId !== "0" ) {
     isOdditySelected = true
     itineraryData.oddityId = customEvent.detail.oddityId
     itineraryData.oddityName = customEvent.detail.oddityName
     render()
   }
+  else {
+    isOdditySelected = false
+  }
 
 })
 eventHub.addEventListener("eaterySelected", customEvent => {
   
-  if ( customEvent.detail.eateryId !== "" ) {
+  if ( customEvent.detail.eateryId !== "0" ) {
     isEaterySelected = true
     itineraryData.eateryId = customEvent.detail.eateryId
     itineraryData.eateryName = customEvent.detail.eateryName
     render()
+  }
+  else {
+    isEaterySelected = false
   }
 
 })
