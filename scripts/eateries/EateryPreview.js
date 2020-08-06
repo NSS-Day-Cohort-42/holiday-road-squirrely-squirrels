9318aa800ea59b9c1ac4e371abcd0e6436102ef6
estherviolin <esthernaomi.violin@gmail.com>
2020-08-06 10:26:28 -0500

    changed button and dialog id to be more specific

--- scripts/eateries/EateryPreview.js
+++ scripts/eateries/EateryPreview.js
@@ -18,11 +18,11 @@ eventHub.addEventListener("eaterySelected", (customEvent) => {
 
 eventHub.addEventListener("click", (clickEvent) => {
     const [prefix, eateryId] = event.target.id.split("--")
-    if (event.target.id.startsWith("showDetail--")) {
-        const dialogTarget = document.querySelector(`.showDialog--${eateryId}`)
+    if (event.target.id.startsWith("showEateryDetail--")) {
+        const dialogTarget = document.querySelector(`.showEateryDialog--${eateryId}`)
         dialogTarget.showModal()
     }
-    else if (event.target.id === `closeDialog--${eateryId}`) {
+    else if (event.target.id === `closeEateryDialog--${eateryId}`) {
         const theDialog = event.target.parentNode
         theDialog.close()
     }
@@ -36,8 +36,8 @@ const render = (selectedEateryObj) => {
     contentTarget.innerHTML = `
         <h3>${selectedEateryObj.businessName}</h3>
         <div>${selectedEateryObj.city}, ${selectedEateryObj.state}</div>
-        <button class="detailButton" id="showDetail--${selectedEateryObj.id}">Details</button>
-        <dialog class="dialog showDialog--${selectedEateryObj.id}">
+        <button class="detailButton" id="showEateryDetail--${selectedEateryObj.id}">Details</button>
+        <dialog class="dialog showEateryDialog--${selectedEateryObj.id}">
             <h4>${selectedEateryObj.businessName}</h4>
             <p>${selectedEateryObj.description}</p>
             <p>pet friendly: ${selectedEateryObj.ameneties.petFriendly}</p>
@@ -45,7 +45,7 @@ const render = (selectedEateryObj) => {
             <p>diaper facility: ${selectedEateryObj.ameneties.diaperFacility}</p>
             <p>playground: ${selectedEateryObj.ameneties.playground}</p>
             <p>restrooms: ${selectedEateryObj.ameneties.restrooms}</p>
-            <button class="closeButton" id="closeDialog--${selectedEateryObj.id}">Close</button
+            <button class="closeButton" id="closeEateryDialog--${selectedEateryObj.id}">Close</button
 
         </dialog>
            
