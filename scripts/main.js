7b3c696f4105a6a69d024a652c30a2644e444556
Jacob Eckert <jweckert17@gmail.com>
2020-08-06 09:46:32 -0500

    actually fixed merge issues

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,8 @@ import "./weather/WeatherPreview.js"
 import { EaterySelect } from "./eateries/EaterySelect.js"
 import { odditySelect } from"./oddities/OdditySelect.js"
 import { ParkSelect } from "./parks/ParkSelect.js"
-<<<<<<< HEAD
 import "./parks/ParkPreview.js"
-=======
 import "./oddities/OddityPreview.js"
->>>>>>> master
 
 
 
