b21d1d017b5f335b4a92c67e41bcb87e44aaec57
estherviolin <esthernaomi.violin@gmail.com>
2020-08-06 10:13:47 -0500

    preview is rendering and dialog box working

--- scripts/eateries/EateryPreview.js
+++ scripts/eateries/EateryPreview.js
@@ -1,4 +1,4 @@
-import { useEateries, getEateries } from "./EateryProvider.js"
+import { useEateries} from "./EateryProvider.js"
 
 const contentTarget = document.querySelector(".preview--eatery")
 const eventHub = document.querySelector(".container")
@@ -8,19 +8,24 @@ const eventHub = document.querySelector(".container")
 
 //listen for custom event from eatery selects
 eventHub.addEventListener("eaterySelected", (customEvent) => {
-
-    const eaterySelected = customEvent.detail.eateryId
-
     const allEateries = useEateries()
+    const eaterySelected = allEateries.find(eatery => {
+        return parseInt(event.detail.eateryId) === eatery.id
+    })
+    render(eaterySelected)
 
-    const selectedEatery = allEateries.find(
-        (eateryObj) => {
-            return eaterySelected === eateryObj.id
-        }
-    ) //returns one object with matching id
+})  
 
-    //add to DOM
-    render
+eventHub.addEventListener("click", (clickEvent) => {
+    const [prefix, eateryId] = event.target.id.split("--")
+    if (event.target.id.startsWith("showDetail--")) {
+        const dialogTarget = document.querySelector(`.showDialog--${eateryId}`)
+        dialogTarget.showModal()
+    }
+    else if (event.target.id === `closeDialog--${eateryId}`) {
+        const theDialog = event.target.parentNode
+        theDialog.close()
+    }
 })
 
 
@@ -31,17 +36,16 @@ const render = (selectedEateryObj) => {
     contentTarget.innerHTML = `
         <h3>${selectedEateryObj.businessName}</h3>
         <div>${selectedEateryObj.city}, ${selectedEateryObj.state}</div>
-        <button class="detailButton" id="detail--${selectedEateryObj.id}">Details</button>
-        <dialog class="dialog dialog--${selectedEateryObj.id}">
+        <button class="detailButton" id="showDetail--${selectedEateryObj.id}">Details</button>
+        <dialog class="dialog showDialog--${selectedEateryObj.id}">
             <h4>${selectedEateryObj.businessName}</h4>
             <p>${selectedEateryObj.description}</p>
-            <p>${selectedEateryObj.ameneties.petFriendly}</p>
-            <p>${selectedEateryObj.ameneties.wifi}</p>
-            <p>${selectedEateryObj.ameneties.diaperFacility}</p>
-            <p>${selectedEateryObj.ameneties.playground}</p>
-            <p>${selectedEateryObj.ameneties.restrooms}</p>
-
-            <button class="closeButton" id="close--${selectedEateryObj.id}">Close</button
+            <p>pet friendly: ${selectedEateryObj.ameneties.petFriendly}</p>
+            <p>wifi: ${selectedEateryObj.ameneties.wifi}</p>
+            <p>diaper facility: ${selectedEateryObj.ameneties.diaperFacility}</p>
+            <p>playground: ${selectedEateryObj.ameneties.playground}</p>
+            <p>restrooms: ${selectedEateryObj.ameneties.restrooms}</p>
+            <button class="closeButton" id="closeDialog--${selectedEateryObj.id}">Close</button
 
         </dialog>
            
--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@ import { EaterySelect } from "./eateries/EaterySelect.js"
 import { odditySelect } from"./oddities/OdditySelect.js"
 import { ParkSelect } from "./parks/ParkSelect.js"
 import "./oddities/OddityPreview.js"
+import "./eateries/EateryPreview.js"
 
 
 
