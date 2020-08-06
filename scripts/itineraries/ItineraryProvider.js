4033ab5f485e609d183b429f9e1f516cdc147f88
Raparker88 <raparker88@gmail.com>
2020-08-06 11:22:12 -0500

    pluralized get itinerary function

--- scripts/itineraries/ItineraryProvider.js
+++ scripts/itineraries/ItineraryProvider.js
@@ -8,7 +8,7 @@ const dispatchStateChangeEvent = () => {
 }
 
 
-export const getItinerary = () => {
+export const getItineraries = () => {
     return fetch("http://localhost:8088/itineraries")
     .then(response => response.json())
     .then(parsedItineraries => {
