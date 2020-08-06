f9b36736a373dbaf6c1aac9c72fe9499976ac19f
Raparker88 <raparker88@gmail.com>
2020-08-06 10:52:08 -0500

    filled in ItineraryProvider.js

---
+++ scripts/itineraries/ItineraryProvider.js
@@ -0,0 +1,33 @@
+const eventHub = document.querySelector(".container")
+
+let itineraries = []
+
+const dispatchStateChangeEvent = () => {
+    const itineraryStateChangedEvent = new CustomEvent("itineraryChange")
+    eventHub.dispatchEvent(itineraryStateChangedEvent)
+}
+
+
+export const getItinerary = () => {
+    return fetch("http://localhost:8088/itineraries")
+    .then(response => response.json())
+    .then(parsedItineraries => {
+        itineraries = parsedItineraries
+    })
+}
+
+export const saveItinerary = itinerary => {
+    return fetch("http://localhost:8088/itineraries", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(itinerary)
+    })
+    .then(getItinerary)
+    .then(dispatchStateChangeEvent)
+}
+
+export const useItineraries = () => {
+    return itineraries.slice()
+}
