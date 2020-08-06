9c0d2ca49233c050686ba90d9fc4ee32fd070185
Jacob Eckert <jweckert17@gmail.com>
2020-08-06 09:43:15 -0500

    a lil less code duplication in the click listener

--- scripts/parks/ParkPreview.js
+++ scripts/parks/ParkPreview.js
@@ -4,15 +4,16 @@ const eventHub = document.querySelector(".container")
 const contentTarget = document.querySelector(".preview--park")
 
 contentTarget.addEventListener("click", event => {
-  if(event.target.id.startsWith("parkDetail--")) {
-    const parkId = event.target.id.split("--")[1]
-
+  if(event.target.id.startsWith("openParkDetail--") || event.target.id.startsWith("closeParkDetail--")) {
+    const [ prefix, parkId ] = event.target.id.split("--")
     const dialogNode = document.querySelector(`#parkDialog--${parkId}`)
-    dialogNode.showModal()
-  }
-  else if(event.target.classList.contains("closeButton")) {
-    const dialogNode = event.target.parentNode
-    dialogNode.close()
+
+    if(prefix === "openParkDetail") {
+      dialogNode.showModal()
+    }
+    else {
+      dialogNode.close()
+    }
   }
 })
 
@@ -20,11 +21,11 @@ const render = park => {
   contentTarget.innerHTML = `
     <h3 class="preview__header">${park.name}</h3>
     <div class="preview__location">${park.addresses[0].city}, ${park.addresses[0].stateCode}</div>
-    <button class="detailButton" id="parkDetail--${park.id}">Details</button>
+    <button class="detailButton" id="openParkDetail--${park.id}">Details</button>
     <dialog class="dialog dialog--park" id="parkDialog--${park.id}">
       <h4 class="preview-dialog__header">${park.name}</h4>
       <p class="preview-dialog__description">${park.description}</p>
-      <button class="closeButton" id="close--${park.id}">Close</button>
+      <button class="closeButton" id="closeParkDetail--${park.id}">Close</button>
     </dialog>
   `
 }
