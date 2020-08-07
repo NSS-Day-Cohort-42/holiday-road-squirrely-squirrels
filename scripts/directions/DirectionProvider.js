751e50648f5ec2428685ad2e5de6dcf136e8ad49
mgjeffries <gib.jeffries@gmail.com>
2020-08-07 15:40:20 -0500

    wip - export function for getting directions

--- scripts/directions/DirectionProvider.js
+++ scripts/directions/DirectionProvider.js
@@ -2,21 +2,39 @@ import  keys  from "../Settings.js";
 
 const startingCity = "Nashville"
 const startingState = "Tn"
+let LatLongPairs = []
 
+export const getDirections = () =>  {
+  getLatLngPairs()
+    .then(getDirectionsFromLatLongs())
+}
 
-
-let results = []
 
 const getLatLngPair = ( placeName ) => {
   return fetch(`https://graphhopper.com/api/1/geocode?q=${placeName}&locale=us&debug=true&key=${keys.graphhopperKey}`)
   .then(response => response.json())
   .then( resultsObject => {
-    results.push(resultsObject.hits["0"].point)
+    LatLongPairs.push(resultsObject.hits["0"].point)
   })
 }
 
-// Testing to see if this works
-getLatLngPair(startingCity)
-  .then( () => getLatLngPair("Boise"))
-  .then( () => console.log(results))
-  
+
+// a function that sends data to LatLongPairs
+const getLatLngPairs = () => {
+  getLatLngPair( startingCity+"+"+startingState ) 
+    .then( () => getLatLngPair("Boise")) //example of gettting latLongs for multiple cities
+    .then( () => getLatLngPair("Boise"))
+    .then( () => getLatLngPair("Boise"))
+    .then( () => getLatLngPair("Boise"))
+    .then( () => getLatLngPair("Boise"))
+    .then( () => getLatLngPair("Boise"))
+    .then( () => getLatLngPair("Boise"))
+    .then( () => console.log(LatLongPairs))
+} 
+
+
+// a function that takes uses LatLongPairs to get directions
+const getDirectionsFromLatLongs = () => {} 
+
+// call get latLong pairs for testing
+getLatLngPairs()
