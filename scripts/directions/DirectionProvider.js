9aceba0bdf80a729383a7def240f7c4e47ef63d7
mgjeffries <gib.jeffries@gmail.com>
2020-08-07 14:44:21 -0500

    get latLng for two places

--- scripts/directions/DirectionProvider.js
+++ scripts/directions/DirectionProvider.js
@@ -3,16 +3,20 @@ import  keys  from "../Settings.js";
 const startingCity = "Nashville"
 const startingState = "Tn"
 
-let result = {}
+
+
+let results = []
 
 const getLatLngPair = ( placeName ) => {
   return fetch(`https://graphhopper.com/api/1/geocode?q=${placeName}&locale=us&debug=true&key=${keys.graphhopperKey}`)
   .then(response => response.json())
   .then( resultsObject => {
-    result = resultsObject.hits["0"].point
+    results.push(resultsObject.hits["0"].point)
   })
 }
 
 // Testing to see if this works
 getLatLngPair(startingCity)
-  .then( () => console.log(result))
+  .then( () => getLatLngPair("Boise"))
+  .then( () => console.log(results))
+  
