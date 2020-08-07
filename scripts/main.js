627a63a67ffadb49ae8e340897c9d68b2afe9d17
mgjeffries <gib.jeffries@gmail.com>
2020-08-07 11:48:13 -0500

    remove save button from initial page load

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ import "./weather/WeatherPreview.js"
 import "./parks/ParkPreview.js"
 import "./oddities/OddityPreview.js"
 import "./eateries/EateryPreview.js"
-import { ItinerarySaveButton } from "./itineraries/ItinerarySaveButton.js"
+import "./itineraries/ItinerarySaveButton.js"
 import { EaterySelect } from "./eateries/EaterySelect.js"
 import { odditySelect } from"./oddities/OdditySelect.js"
 import { ParkSelect } from "./parks/ParkSelect.js"
@@ -11,7 +11,6 @@ import {ItineraryList} from "./itineraries/ItineraryList.js"
 
 ParkSelect()
 odditySelect()
-ItinerarySaveButton()
 EaterySelect()
 ItineraryList()
 
