c1f31f70ba47c545746afaaf565ceb202379ab59
Jacob Eckert <jweckert17@gmail.com>
2020-08-10 10:46:01 -0500

    added header to direction list. imported direction list in main

--- scripts/directions/DirectionList.js
+++ scripts/directions/DirectionList.js
@@ -9,12 +9,19 @@ eventHub.addEventListener("directionsClicked", event => {
   getDirections(itineraryObj)
     .then(() => {
       const directions = useDirections()
-      render(directions)
+      render(directions, itineraryObj)
     })
 })
 
-const render = directions => {
+const render = (directions, itineraryObj) => {
+  const parkName = itineraryObj.parkInfo.parkName
+  const oddityName = itineraryObj.oddityInfo.oddityName
+  const eateryName = itineraryObj.eateryInfo.eateryName
+
+  const headerText = `Directions from Nashville to ${parkName} to ${oddityName} to ${eateryName}`
+
   contentTarget.innerHTML = `
+    <h3 class="direcitonList__header">${headerText}</h3>
     <ol class="directionList">
       ${directions.map(direction => `<li class="direction">${direction}</li>` )}
     </ol>
